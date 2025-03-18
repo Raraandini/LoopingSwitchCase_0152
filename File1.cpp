4469f883d912258eea66571509b6d2fe571d01e1
Raraandinii <raraandinib@gmail.com>
2025-03-18 21:50:42 +0700

    Pencacahan Turun

--- File1.cpp
+++ File1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
-    
-
-    //Pencacahan Turun
-    for (i = 5; i > 0; i--){
+    //Pencacahan Naik
+    int i;
+    for (i = 0; i < 5; i++){
         cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
-    }  
+    }
+    cout <<"Nilai i terakhir : " << i << endl;
+
+    
 }
