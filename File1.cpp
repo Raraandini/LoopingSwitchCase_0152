fa355ba30507a9802cfc2be843044e746a76a712
Raraandinii <raraandinib@gmail.com>
2025-03-18 20:23:26 +0700

    Membuat program perulangan for

---
+++ File1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Pencacahan Naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout <<"Nilai i terakhir : " << i << endl;
+
+    //Pencacahan Turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }  
+}
